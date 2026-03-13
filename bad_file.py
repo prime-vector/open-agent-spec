30dfcbaed661eb8d7799a592a1855ff4043f1cdd
Andrew whitehouse <andrewswhitehouse@gmail.com>
2026-03-13 15:17:52 +1100

    feat: introduce an error

---
+++ bad_file.py
@@ -0,0 +1,6 @@
+import os, sys  # unused imports, multiple on one line
+
+
+def  bad_function(  x:int ,  y:int)->int:   # spacing / formatting violations
+    foo =  1+2
+    return  x+y+foo  # probably fine logically, but style is bad
